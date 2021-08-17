9f503bac774e54aa84bb7fc4051402934c4422db
Gustavo Alessandro <mxgugaa@gmail.com>
2021-08-17 13:54:24 -0700

    Variáveis

---
+++ 02-variaveis.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Meus dados
+$nome = "Gustavo Alessandro";
+$idade = 22;
+$altura = 1.83;
+
+echo "Meu nome é $nome, minha idade é $idade, e minhas altura é $altura <br>";
+
+//Alterando meus dados
+$nome = "Felipe cabral";
+$idade = 29;
+$altura = 1.73;
+
+echo "Meu nome é $nome, minha idade é $idade, e minhas altura é $altura";
+echo "<hr>"
+
+/* Nomes válidos (de variaveis)
+$nome
+$_meunome
+$carro10
+$nota1
+$meu_nome
+$meuNome
+ 
+Nomes inválidos
+$12345nome
+$carro$%¨&
+$meu carro
+*/
+
+?>
