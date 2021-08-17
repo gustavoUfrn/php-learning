edee2b27497fa3db7fc61f41e1073ee1340d04d1
Gustavo Alessandro <mxgugaa@gmail.com>
2021-08-17 13:42:55 -0700

    Fundamentos básicos

---
+++ 01-fundamentos_basicos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Fundamentos básicos php</title>
+</head>
+<body>
+	
+<?php
+//Este é um comentario, ele é iniciado por "//"
+/*
+Este também é um comentario
+*/
+echo "Olá, mundo! <br>";
+print "Este é meu primeiro codigo php";
+
+?>
+
+</body>
+</html>
