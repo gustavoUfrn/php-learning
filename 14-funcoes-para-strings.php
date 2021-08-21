c02595dcdad26cea6ed8b0bcfaaf55418f3282bd
Gustavo Alessandro <mxgugaa@gmail.com>
2021-08-20 17:15:11 -0700

    funçoes para números e strings

--- 14-funcoes-para-strings.php
+++ 14-funcoes-para-strings.php
@@ -17,11 +17,31 @@ $mensagem = "Olá mundo!";
 echo substr($mensagem, 1, 5);
 echo "<br>";
 
-// strpad
+// str_pad
 $completarEspaco = "Qualq";
 $espacoCompleto = str_pad($completarEspaco, 8, ".", STR_PAD_LEFT);
 // LEFT, RIGHT, BOTH, or nothing
 echo $espacoCompleto;
 echo "<br>";
+
+// str_repeat
+$string = str_repeat("Amongus", 4);
+echo $string;
+echo "<br>";
+
+// strlen
+echo strlen($string);
+echo "<br>";
+
+// str_replace
+$texto = "Olá, meu nome é Rogerio";
+$novoTexto = str_replace("Rogerio", "Gustavo", $texto);
+
+echo $novoTexto;
+echo "<br>";
+
+// strpos
+// Posição inicial to texto inserido
+echo strpos($novoTexto, "Gustavo");
 
 ?>
---
+++ 15-funcoes-para-numeros.php
@@ -0,0 +1,24 @@
+<?php
+
+$number = 1234.56;
+$preco = number_format($number, 2,",", ".");
+// número, casas decimais(2), separador decimal(,), separador de milhares(.)
+echo "O valor do produto é $preco";
+echo "<br>";
+
+echo round(3.2);
+echo "<br>";
+echo round(3.49);
+echo "<br>";
+echo round(3.51);
+echo "<br>";
+echo round(3.8);
+echo "<br>";
+
+echo ceil(4.000000000001);
+echo "<br>";
+echo floor(4.999999999999);
+echo "<br>";
+
+echo rand(1,10);
+?>
